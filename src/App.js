91371497224f298552c2cc2a0fe3137f2924c9c8
Maciek C <mac.celinski@gmail.com>
2021-03-09 12:00:01 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-function App() {
-  return (
-    <div className="App">
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, isLogged } from "./store/actions/index";
 
-    </div>
-  );
+function App() {
+	// store state
+	const counter = useSelector((state) => state.counter);
+	const logged = useSelector((state) => state.isLogged);
+	const dispatch = useDispatch();
+
+	return (
+		<div className="App">
+			<h1>Counter: {counter}</h1>
+			<button onClick={() => dispatch(increment(5))}>+</button>
+			<button onClick={() => dispatch(decrement())}>-</button>
+			<button onClick={() => dispatch(isLogged())}>
+				I want to see secrets
+			</button>
+
+			{logged && <h3>Valuable information I shouldn't see</h3>}
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
+// import PROVIDER
+import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//  import REDUCERS
+import rootReducer from "./store/reducers/index";
+
+// import STORE
+import { createStore } from "redux";
+
+// init STORE
+let store = createStore(
+	rootReducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
+ReactDOM.render(
+	// add PROVIDER
+	<Provider store={store}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</Provider>,
+	document.getElementById("root")
+);
+
+// // ACTIONS
+
+// // action increment
+// const increment = () => {
+// 	return {
+// 		type: "INCREMENT",
+// 	};
+// };
+// // action decrement
+// const decrement = () => {
+// 	return {
+// 		type: "DECREMENT",
+// 	};
+// };
+
+// // REDUCER - mutation in vue
+// const counter = (state = 0, action) => {
+// 	switch (action.type) {
+// 		case "INCREMENT":
+// 			return state + 1;
+// 		case "DECREMENT":
+// 			return state - 1;
+// 	}
+// };
+
+// // STORE -> globalized state
+// let store = createStore(counter);
+
+// // dispatch - for dispatching an action
+// store.dispatch(increment());
